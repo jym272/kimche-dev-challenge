7d09309d70f961cefac278a00e0ec75466a27469
jorge <jym272@pop-os.localdomain>
2022-05-10 18:42:04 -0300

    build(graphql): schema added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
+import {gql, useQuery} from "@apollo/client";
+
+const EXCHANGE_RATES = gql`
+    query GetExchangeRates {
+        rates(currency: "USD") {
+            currency
+            rate
+        }
+    }
+`;
 
 function App() {
-  return <>
-    Hiola mundfo
-  </>
+
+    const {loading, error, data} = useQuery(EXCHANGE_RATES);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+
+    const dataRows = data.rates.map(({currency, rate}) => {
+        return <div key={currency}>
+            <p>
+                {currency}: {rate}
+            </p>
+        </div>
+    });
+
+
+    // const dataRows = data.rates.map({currency, rate}) => {
+    //   return <tr>
+    //     <td>{currency}</td>
+    //     <td>{rate}</td>
+    //   </tr>
+    // }
+
+
+    return <>
+        Hiola mundfo
+        {dataRows}
+    </>
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
+
+const client = new ApolloClient(
+    {
+        connectToDevTools: true,
+        uri: 'https://48p1r2roz4.sse.codesandbox.io',
+        cache: new InMemoryCache()
+    });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ApolloProvider client={client}>
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    </ApolloProvider>
 );
 
